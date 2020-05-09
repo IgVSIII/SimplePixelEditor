8f78b5adcd10f43b98dbf4cc01057de1441760ff
IgVSIII <40248988+IgVSIII@users.noreply.github.com>
2020-05-09 20:05:30 +0300

    Updates

---
+++ precache-manifest.3461a4065ec66a41af611f9d5e9e720f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fc920b140ae2d1d29064742cf046ecb",
+    "url": "/SimplePixelEditor/index.html"
+  },
+  {
+    "revision": "6e2a9bf1358f9cd5eb35",
+    "url": "/SimplePixelEditor/static/css/2.35fcc229.chunk.css"
+  },
+  {
+    "revision": "af62f08d6a3ade8bc0b1",
+    "url": "/SimplePixelEditor/static/css/main.5e99716e.chunk.css"
+  },
+  {
+    "revision": "6e2a9bf1358f9cd5eb35",
+    "url": "/SimplePixelEditor/static/js/2.f77e00fd.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/SimplePixelEditor/static/js/2.f77e00fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af62f08d6a3ade8bc0b1",
+    "url": "/SimplePixelEditor/static/js/main.047f4c0e.chunk.js"
+  },
+  {
+    "revision": "ceee627bf78ea20b7ac6",
+    "url": "/SimplePixelEditor/static/js/runtime-main.6e494c86.js"
+  }
+]);
